9bbc5eb41220bd4c9adb3b1b690e5fd073a02fab
Noah Levenson <noahlevenson@gmail.com>
2019-12-21 14:40:20 -0500

    Introduce containers

--- src/mattr.js
+++ src/mattr.js
@@ -1,3 +1,5 @@
+const MTypeData = require("./mcontainer.js").MTypeData;
+
 class MStunAttr {
 	// TODO: Validation
 	constructor(type, len, val) {
@@ -32,24 +34,24 @@ class MStunAttr {
 	};
 
 	static K_ATTR_TYPE_TABLE = new Map([
-		[new Buffer.from([0x00, 0x00]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0000, copt: false}],
-		[new Buffer.from([0x00, 0x01]).toString("hex"), {type: this.K_ATTR_TYPE.MAPPED_ADDRESS, copt: false}],
-		[new Buffer.from([0x00, 0x02]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0002, copt: false}],
-		[new Buffer.from([0x00, 0x03]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0003, copt: false}],
-		[new Buffer.from([0x00, 0x04]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0004, copt: false}],
-		[new Buffer.from([0x00, 0x05]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0005, copt: false}],
-		[new Buffer.from([0x00, 0x06]).toString("hex"), {type: this.K_ATTR_TYPE.USERNAME, copt: false}],
-		[new Buffer.from([0x00, 0x07]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_0007, copt: false}],
-		[new Buffer.from([0x00, 0x08]).toString("hex"), {type: this.K_ATTR_TYPE.MESSAGE_INTEGRITY, copt: false}],
-		[new Buffer.from([0x00, 0x09]).toString("hex"), {type: this.K_ATTR_TYPE.ERROR_CODE, copt: false}],
-		[new Buffer.from([0x00, 0x0A]).toString("hex"), {type: this.K_ATTR_TYPE.UNKNOWN_ATTRIBUTES, copt: false}],
-		[new Buffer.from([0x00, 0x0B]).toString("hex"), {type: this.K_ATTR_TYPE.RESERVED_000B, copt: false}],
-		[new Buffer.from([0x00, 0x14]).toString("hex"), {type: this.K_ATTR_TYPE.REALM, copt: false}],
-		[new Buffer.from([0x00, 0x15]).toString("hex"), {type: this.K_ATTR_TYPE.NONCE, copt: false}],
-		[new Buffer.from([0x00, 0x20]).toString("hex"), {type: this.K_ATTR_TYPE.XOR_MAPPED_ADDRESS, copt: false}],
-		[new Buffer.from([0x80, 0x22]).toString("hex"), {type: this.K_ATTR_TYPE.SOFTWARE, copt: true}],
-		[new Buffer.from([0x80, 0x23]).toString("hex"), {type: this.K_ATTR_TYPE.ALTERNATE_SERVER, copt: true}],
-		[new Buffer.from([0x80, 0x28]).toString("hex"), {type: this.K_ATTR_TYPE.FINGERPRINT, copt: true}]
+		[new Buffer.from([0x00, 0x00]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0000, false, new Buffer.from([0x00, 0x00]))],
+		[new Buffer.from([0x00, 0x01]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.MAPPED_ADDRESS, false, new Buffer.from([0x00, 0x01]))],
+		[new Buffer.from([0x00, 0x02]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0002, false, new Buffer.from([0x00, 0x02]))],
+		[new Buffer.from([0x00, 0x03]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0003, false, new Buffer.from([0x00, 0x03]))],
+		[new Buffer.from([0x00, 0x04]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0004, false, new Buffer.from([0x00, 0x04]))],
+		[new Buffer.from([0x00, 0x05]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0005, false, new Buffer.from([0x00, 0x05]))],
+		[new Buffer.from([0x00, 0x06]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.USERNAME, false, new Buffer.from([0x00, 0x06]))],
+		[new Buffer.from([0x00, 0x07]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_0007, false, new Buffer.from([0x00, 0x07]))],
+		[new Buffer.from([0x00, 0x08]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.MESSAGE_INTEGRITY, false, new Buffer.from([0x00, 0x08]))],
+		[new Buffer.from([0x00, 0x09]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.ERROR_CODE, false, new Buffer.from([0x00, 0x09]))],
+		[new Buffer.from([0x00, 0x0A]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.UNKNOWN_ATTRIBUTES, false, new Buffer.from([0x00, 0x0A]))],
+		[new Buffer.from([0x00, 0x0B]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.RESERVED_000B, false, new Buffer.from([0x00, 0x0B]))],
+		[new Buffer.from([0x00, 0x14]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.REALM, false, new Buffer.from([0x00, 0x14]))],
+		[new Buffer.from([0x00, 0x15]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.NONCE, false, new Buffer.from([0x00, 0x15]))],
+		[new Buffer.from([0x00, 0x20]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.XOR_MAPPED_ADDRESS, false, new Buffer.from([0x00, 0x20]))],
+		[new Buffer.from([0x80, 0x22]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.SOFTWARE, true, new Buffer.from([0x80, 0x22]))],
+		[new Buffer.from([0x80, 0x23]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.ALTERNATE_SERVER, true, new Buffer.from([0x80, 0x23]))],
+		[new Buffer.from([0x80, 0x28]).toString("hex"), new MTypeData(this.K_ATTR_TYPE.FINGERPRINT, true, new Buffer.from([0x80, 0x28]))]
 	]);
 
 	static K_ADDR_FAMILY = {
@@ -69,13 +71,17 @@ class MStunAttr {
 			return dtype;
 		}
 		
-		return this.K_MSG_TYPE.MALFORMED;
+		return {type: this.K_ATTR_TYPE.MALFORMED, copt: null, buf: null};
 	}
 
 	static decLen(len) {
 		const buf = Uint8Array.from(len);
 		const view = new Uint16Array(buf.buffer);
 		return view[0];
+	}
+
+	static enType(type) {
+
 	}
 
 	static enLen(len) {
---
+++ src/mcontainer.js
@@ -0,0 +1,9 @@
+class MTypeData {
+	constructor(type, data, bin) {
+		this.type = type;
+		this.data = data;
+		this.bin = bin;
+	}
+}
+
+module.exports.MTypeData = MTypeData;
